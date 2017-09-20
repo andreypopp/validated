d4fdad57b764883e786985085b1033e429d31133
sean lee <freshdried@gmail.com>
2017-09-20 15:32:39 -0400

    Update schema.js

--- src/schema.js
+++ src/schema.js
@@ -387,7 +387,7 @@ export function enumeration(...values: Array<mixed>) {
   return node;
 }
 
-export class OneOfNode<A> extends Node<A> {
+export class OneOfNode<V> extends Node<V> {
 
   nodes: Array<Node<*>>;
 
